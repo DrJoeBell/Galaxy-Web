7dcd9e062dbce58d3873f66cd10b6b93ba0d9ea7
Clement RICHARD <clement.richard443@gmail.com>
2013-12-19 13:58:58 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,5 @@
+Modèle-Vue-Contrôleur: une méthode de conception de logiciel. 
+Modèle d'architecture qui cherche à séparer nettement les couches de présentation (UI : User Interface),
+ métier (BLL : Business Logic Layer) et d'accès aux données (DAL : Data Access Layer).
+  Le but étant d'avoir une dépendance minimale entre les différentes couches de l'application ;
+   ainsi les modifications effectuées sur n'importe quelle couche de l’application n'affectent pas les autres couches.
